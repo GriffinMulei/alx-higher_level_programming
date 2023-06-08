417101d32bcfc74a2f161de6b087d3af264305ca
GriffinMulei <muleigriffin@gmail.com>
2023-06-08 12:10:26 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if count == 0:
 elif count == 1:
     print("1 argument:")
 else:
-    print("{} arguments:".format(count))
+    print(f"{count} arguments:".format(count))
 for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+     print("f{i + 1}: {sys.argv}".format(i + 1, sys.argv[i + 1]))
