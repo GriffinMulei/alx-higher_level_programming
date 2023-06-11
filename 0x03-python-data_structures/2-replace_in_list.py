07b5c53958c81a038770468a1fd6958115dc4727
GriffinMulei <muleigriffin@gmail.com>
2023-06-11 00:28:33 -0700

    1-element_at.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
+my_list = [1, 2, 3, 4, 5]
+def replace_in_list(my_list, idx, element) - 1:
+    if idx >= 0 and idx < len(my_list) - 1:
         return my_list
     else:
         my_list[idx] = element
