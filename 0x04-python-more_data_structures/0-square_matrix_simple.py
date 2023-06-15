1c187a3d54b9d3a45e8ad52d9258d07ceda9a144
GriffinMulei <muleigriffin@gmail.com>
2023-06-15 00:25:59 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def squre_matrix_simple(matrix=[]):
+    """
+    wordA function that computes the squre
+    value of all integers of a matrix.
+    """
+    new matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x***2, col))
+        new_matrix.append(results)
+    return new_matrix
