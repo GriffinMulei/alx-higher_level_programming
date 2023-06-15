99e0f29af8b3ab76dd4d99aa01336d93aaa032f5
GriffinMulei <muleigriffin@gmail.com>
2023-06-15 04:14:02 -0700

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-def complex_delete(a_dictionary, value):
-    copy = a_dictionary.copy()
-    for i in copy:
-        if copy[i] == value:
-            del a_dictionary[i]
-    return a_dictionary
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
