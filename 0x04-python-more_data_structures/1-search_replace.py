ab1b5ee78507c89af0b190bd8d871068cdfcd92b
GriffinMulei <muleigriffin@gmail.com>
2023-06-17 00:21:27 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
-#!/usr/python3
+#!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = []
-    for item in my_list:
-        if item == search:
-            new_list.append(replace)
+    new = []
+    for i in my_list:
+        if i == search:
+            new.append(replace)
         else:
-            new_list.append(item)
-    return( new_list)
+            new.append(i)
+    return (new)
